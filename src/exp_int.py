3259bc09a50c2551157c33a7018ce0e01347f462
Jonathan Gosling <jonathangosiling@kubrickgroup.com>
2023-08-09 14:26:04 +0100

    updated source code to use custom and pypi modules/packages

---
+++ src/exp_int.py
@@ -0,0 +1,4 @@
+import scipy
+
+def get_exp_integral(x):
+    print(f"The expotential integral of {x} is {scipy.special.expi(x)}")
--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 import json
+from exp_int import get_exp_integral
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda after change no 2!')
-    }
+    if event["value"] != None:
+        try:
+            x = float(event["value"])
+            return {'statusCode': 200,
+                    'body': json.dumps(get_exp_integral(x))
+            }
+
+        except:
+            return {'statusCode': 500,
+                    'body': json.dumps("Incompatible value passed")
+            }
+    else:
+        return {'statusCode': 200,
+                'body': json.dumps('''Supply a value as a query parameter
+                                      to get the exponential integral e.g. 
+                                      "value=1"''')
+        }
